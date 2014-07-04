ac5a1f193f0eac2c37fd089dc2657ff58fdce76d
zxc123zxc <mark.martynov@yahoo.com>
2014-07-04 09:06:02 +0400

    initial commit

---
+++ Epam.ScrumPocker.WebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Epam.ScrumPocker.WebApplication.Startup))]
+namespace Epam.ScrumPocker.WebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+        }
+    }
+}
